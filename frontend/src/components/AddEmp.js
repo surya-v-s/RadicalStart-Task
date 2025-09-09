1fa353b513806ecdca38bcb9bcba2c6fa54a378e
Surya V S <v.s.surya007@gmail.com>
2025-09-09 13:08:35 +0530

    added dropdown menu

--- frontend/src/components/AddEmp.js
+++ frontend/src/components/AddEmp.js
@@ -15,18 +15,64 @@ function AddEmp(){
         }).then(res => {if(res.ok) {alert("Employee Added");navigate("/")} }).catch(err => console.error("Error:",err))
    }
 
+   const handleChange = (e) =>{
+        setEmpInfo({...empInfo,[e.target.name]:e.target.value})
+   }
+
     return(
         <>
             <h1>Add Employee</h1>
             <form onSubmit={handleSubmit}>
-            <label>Name : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,name:e.target.value} })} } type="text" name = "name" placeholder="Enter Name"/></label>
-            <label>Employee ID :  <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,emp_id:e.target.value} })} } type="number" name = "emp_id" placeholder="Enter ID"/></label><br/><br/>
-            <label>Department : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,dept:e.target.value} })} } type="text" name = "dept" placeholder="Enter Department"/></label>
-            <label>Designation : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,desgn:e.target.value} })} } type="text" name = "desgn" placeholder="Enter Designation"/></label><br/><br/>
-            <label>Project : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,project:e.target.value} })} } type="text" name = "project" placeholder="Enter Project"/></label>
-            <label>Type : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,type:e.target.value} })} } type="text" name = "type" placeholder="Enter Type"/></label><br/><br/>
-            <label>Status : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,status:e.target.value} })} } type="text" name = "status" placeholder="Enter Status"/></label><br/><br/>
-            <input type="submit" value="Confirm"/>
+                
+                <label>Name : <input onChange={handleChange} type="text" name = "name" placeholder="Enter Name" required/></label><br/><br/>
+                <label>Employee ID :  <input onChange={handleChange} type="number" name = "emp_id" placeholder="Enter ID" required/></label><br/><br/>
+                
+                <label>Department : 
+                    <select value={empInfo.dept} name="dept" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Design">Design</option>
+                        <option value="Development">Development</option>
+                        <option value="Testing">Testing</option>
+                    </select>
+                </label><br/><br/>
+
+                <label>Designation :  
+                    <select value={empInfo.desgn} name="desgn" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Design Lead">Design Lead</option>
+                        <option value="Senior Developer">Senior Developer</option>
+                        <option value="Junior Developer">Junior Developer</option>
+                        <option value="Senior Test Engineer">Senior Test Engineer</option>
+                        <option value="Junior Test Engineer">Junior Test Engineer</option>
+                    </select>
+                </label><br/><br/>
+
+                <label>Project : <input onChange={handleChange} type="text" name = "project" placeholder="Enter Project" required/></label>
+                <br/><br/>
+
+                <label>Type : 
+                    <select value={empInfo.type} name="type" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Office">Office</option>
+                        <option value="Home">Home</option>
+                        <option value="Hybrid">Hybrid</option>
+                    </select>
+                </label><br/><br/>
+
+                <label>Status : 
+                    <select value={empInfo.status} name="status" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Permanent">Permanent</option>
+                        <option value="Temporary">Temporary</option>
+                    </select>
+                </label><br/><br/>
+                {/* <label>Department : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,dept:e.target.value} })} } type="text" name = "dept" placeholder="Enter Department"/></label> */}
+                {/* <label>Designation : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,desgn:e.target.value} })} } type="text" name = "desgn" placeholder="Enter Designation"/></label><br/><br/> */}
+                
+                {/* <label>Type : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,type:e.target.value} })} } type="text" name = "type" placeholder="Enter Type"/></label><br/><br/> */}
+                {/* <label>Status : <input onChange={ (e) => { setEmpInfo((prev) => { return {...prev,status:e.target.value} })} } type="text" name = "status" placeholder="Enter Status"/></label><br/><br/> */}
+                <input type="submit" value="Confirm"/>
+           
             </form>
         </>
     )
--- frontend/src/components/EditEmp.js
+++ frontend/src/components/EditEmp.js
@@ -10,7 +10,7 @@ function EditEmp(){
         fetch(`http://localhost:5000/emp/${id}`).then(res=>res.json())
         .then(data=>setInfo(data)).catch(err=>console.log("error1:",err))
 
-    },[])
+    },[id])
 
     const handleSubmit=(e) =>{
         e.preventDefault()
@@ -25,7 +25,10 @@ function EditEmp(){
         setInfo({
             ...info,[e.target.name]:e.target.value
         })
+        
     }
+    
+    
 
     return(
         <>
@@ -33,11 +36,52 @@ function EditEmp(){
             <form onSubmit={handleSubmit}>
                 <label>Name:<input type="text" name="name" value={info.name} onChange={handleChange}/></label><br/><br/>
                 <label>Employee ID :<input name="emp_id" type="text" value={info.emp_id} onChange={handleChange}/></label><br/><br/>
-                <label>Designation:<input name="desgn" type="text" value={info.desgn} onChange={handleChange}/></label><br/><br/>
-                <label>Department:<input name="dept" type="text" value={info.dept} onChange={handleChange}/></label><br/><br/>
+                
+                {/* <label>Designation:<input name="desgn" type="text" value={info.desgn} onChange={handleChange}/></label><br/><br/> */}
+                 
+                 <label>Designation :  
+                    <select value={info.desgn}  name="desgn" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Design Lead">Design Lead</option>
+                        <option value="Senior Developer">Senior Developer</option>
+                        <option value="Junior Developer">Junior Developer</option>
+                        <option value="Senior Test Engineer">Senior Test Engineer</option>
+                        <option value="Junior Test Engineer">Junior Test Engineer</option>
+                    </select>
+                </label><br/><br/>
+
+                <label>Department : 
+                    <select value={info.dept} name="dept" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Design">Design</option>
+                        <option value="Development">Development</option>
+                        <option value="Testing">Testing</option>
+                    </select>
+                </label><br/><br/>
+
+                {/* <label>Department:<input name="dept" type="text" value={info.dept} onChange={handleChange}/></label><br/><br/> */}
                 <label>Project :<input name="project" type="text" value={info.project} onChange={handleChange}/></label><br/><br/>
-                <label>Type :<input name="type" type="text" value={info.type} onChange={handleChange}/></label><br/><br/>
-                <label>Status :<input name="status" type="text" value={info.status} onChange={handleChange}/></label><br/><br/>
+                
+                {/* <label>Type :<input name="type" type="text" value={info.type} onChange={handleChange}/></label><br/><br/> */}
+                {/* <label>Status :<input name="status" type="text" value={info.status} onChange={handleChange}/></label><br/><br/> */}
+                
+                <label>Type : 
+                    <select value={info.type} name="type" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Office">Office</option>
+                        <option value="Home">Home</option>
+                        <option value="Hybrid">Hybrid</option>
+                    </select>
+                </label><br/><br/>
+
+                <label>Status : 
+                    <select value={info.status} name="status" onChange={handleChange} required>
+                        <option value="">Select</option>
+                        <option value="Permanent">Permanent</option>
+                        <option value="Temporary">Temporary</option>
+                    </select>
+                </label><br/><br/>
+
                 <input type="submit" value="Update"/>
 
             </form>
